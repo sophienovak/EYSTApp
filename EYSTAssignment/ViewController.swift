dd0741b4c80ca0389c51699980f2868d85c2173b
Sophie Novak <sophienovak@sophies-macbook-air-2.local>
2019-06-23 20:53:33 +0100

    Initial Commit

---
+++ EYSTAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EYSTAssignment
+//
+//  Created by Sophie Novak on 27/03/2018.
+//  Copyright © 2018 Sophie Novak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
